91886f3fbfbc449743ddf1392f248296db4dd6a1
Sebastian Loewe <66331097+SebastianLoewe@users.noreply.github.com>
2021-07-04 12:54:31 +0200

    Create sketch.js

---
+++ machine_learning/posenet_eye_circles/sketch.js
@@ -0,0 +1,70 @@
+// Copyright (c) 2019 ml5
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+/* ===
+ml5 Example
+PoseNet example using p5.js
+=== */
+
+let video;
+let poseNet;
+let poses = [];
+
+function setup() {
+  createCanvas(640, 480);
+  video = createCapture(VIDEO);
+  video.size(width, height);
+  
+
+  // Create a new poseNet method with a single detection
+  poseNet = ml5.poseNet(video, modelReady);
+  // This sets up an event that fills the global variable "poses"
+  // with an array every time new poses are detected
+  poseNet.on('pose', function(results) {
+    poses = results;
+  });
+  // Hide the video element, and just show the canvas
+  video.hide();
+}
+
+function modelReady() {
+  select('#status').html('Model Loaded');
+}
+
+function draw() {
+  //move image by the width of image to the left
+  translate(video.width, 0);
+  //then scale it by -1 in the x-axis
+  //to flip the image
+  scale(-1, 1);
+  //draw video capture feed as image inside p5 canvas
+  image(video, 0, 0);
+  
+  //filter(INVERT);
+  strokeWeight(2);
+
+  // For one pose only (use a for loop for multiple poses!)
+  if (poses.length > 0) {
+    let pose = poses[0].pose;
+
+    // Create a pink ellipse for the nose
+    fill(213, 0, 143);
+    let nose = pose['nose'];
+    ellipse(nose.x, nose.y, 20, 20);
+
+    // Create a yellow ellipse for the right eye
+    fill(255, 215, 0);
+    let rightEye = pose['rightEye'];
+    ellipse(rightEye.x, rightEye.y, 20, 20);
+
+    // Create a yellow ellipse for the left eye
+    fill(255, 215, 0);
+    let leftEye = pose['leftEye'];
+    ellipse(leftEye.x, leftEye.y, 20, 20);
+    
+    eyeDistance = dist(rightEye.x, rightEye.y, leftEye.x, leftEye.y);
+    //print(eyeDistance);
+  }
+}
